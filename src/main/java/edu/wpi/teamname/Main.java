b5f6d75893f26850b618794922d23e0619f25e0f
Ryan Harris <dinofossil10@verizon.net>
2021-03-25 15:18:58 -0400

    Added print out for own name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -10,8 +10,7 @@ public class Main {
     App.launch(App.class, args);
      */
     System.out.println("Team Member Names Below:");
-    System.out.println("Michael Clements");
-    System.out.println("Team Coach: Michael Clements");
+    System.out.println("Ryan Harris");
 
   }
 }
