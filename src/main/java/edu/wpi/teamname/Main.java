5d7381c5df194aa5e73539ccc85f36247dc89a71
ananya <ananya.gopalan@gmail.com>
2021-04-01 18:57:53 -0400

    made changes

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -9,26 +9,10 @@ public class Main {
     to add code directly to this file.
     App.launch(App.class, args);
      */
-    System.out.println("Team Member Names Below:");
-    System.out.println("Kush Shah");
-    System.out.println("Lauren Sowerbutts");
-    System.out.println("Sai Vadlamudi");
-    System.out.println("Ryan Harris");
-    System.out.println("Ananya Gopalan");
-    System.out.println("Finn Wander");
-    System.out.println("Nina Taurich");
 
-    System.out.println("In Branch Sai");
 
 
     System.out.println("In Branch Ananya");
 
-    System.out.println("In Branch Lauren");
-
-
-    System.out.println("In Branch Ryan");
-
-    System.out.println("In Branch Kush");
-    System.out.println("In Branch Nina");
   }
 }
